d8ba87be001e553172c84cb2f39f80e2298517f6
prettysavagedeveloper <prettysavagedeveloper@gmail.com>
2021-04-14 17:29:30 +0200

    order test for - display the whole menu - the selected menu

--- test/com.prettysavagedeveloper/OrderTest.java
+++ test/com.prettysavagedeveloper/OrderTest.java
@@ -1,1 +1,37 @@
-package prettysavagedeveloper;
+package com.prettysavagedeveloper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrderTest{
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams(){
+        System.setOut(System.out);
+    }
+
+    Order order = new Order();
+
+    @Test
+    public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText(){
+        order.displayAvailableMenu();
+        assertEquals(false, outContent.toString().isEmpty());
+    }
+
+    @Test
+    public void Given_NasiLemak_When_AskForSelectedMenu(){
+        order.displaySelectedMenu(2);
+        assertEquals("You have choosen Menu 2\n", outContent.toString().replace("\r\n", "\n"));
+    }
+}
