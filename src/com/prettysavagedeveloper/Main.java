18dfaa7843a1125fb141d8fd589a4103857d8ad3
prettysavagedeveloper <prettysavagedeveloper@gmail.com>
2021-04-14 16:39:51 +0200

    added in the main file the following - methods objects for displaying the menu and the selected menu - user input to get the menu number

--- src/com/prettysavagedeveloper/Main.java
+++ src/com/prettysavagedeveloper/Main.java
@@ -1,8 +1,16 @@
 package com.prettysavagedeveloper;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Order order = new Order();
+        order.displayAvailableMenu();
+        Scanner sc= new Scanner(System.in);
+        int nb = sc.nextInt();
+        order.displaySelectedMenu(nb);
     }
 }
+
+
