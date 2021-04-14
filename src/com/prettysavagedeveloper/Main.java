9189d89854c1bfbfad3488da96de4c5060d3a2ca
prettysavagedeveloper <prettysavagedeveloper@gmail.com>
2021-04-14 18:38:05 +0200

    simplified main menu

--- src/com/prettysavagedeveloper/Main.java
+++ src/com/prettysavagedeveloper/Main.java
@@ -1,15 +1,10 @@
 package com.prettysavagedeveloper;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         Order order = new Order();
-        order.displayAvailableMenu();
-        Scanner sc= new Scanner(System.in);
-        int nb = sc.nextInt();
-        order.displaySelectedMenu(nb);
+        order.runMenu();
     }
 }
 
