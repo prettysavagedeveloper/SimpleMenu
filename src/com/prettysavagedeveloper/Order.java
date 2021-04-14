eac5f336a192519241ad88d75eab72f3add54d54
prettysavagedeveloper <prettysavagedeveloper@gmail.com>
2021-04-14 18:04:36 +0200

    implemented behaviors of order file to - display the menu - display information according to the menu selected

--- src/com/prettysavagedeveloper/Order.java
+++ src/com/prettysavagedeveloper/Order.java
@@ -3,16 +3,30 @@ package com.prettysavagedeveloper;
 public class Order {
     /* Display all the menu in the restaurant */
 
-    public void displayAvailableMenu(){
+    public void displayAvailableMenu() {
 
         System.out.println("Menu choice");
-        System.out.println("Menu 1: Rendang Beef");
+        System.out.println("Menu 1: Beef Rendang");
         System.out.println("Menu 2: Nasi Lemak");
         System.out.println("Menu 3: Mee Goreng");
         System.out.println("Which menu you desired?");
     }
+
     /*Display the selected menu */
-    public void displaySelectedMenu(int nbMenu){
-        System.out.println("You have choosen Menu " + nbMenu);
+    public void displaySelectedMenu(int nbMenu) {
+        switch (nbMenu) {
+            case 1:
+                System.out.println("You have chosen Menu Beef Rendang");
+                break;
+            case 2:
+                System.out.println("You have chosen Menu Nasi Lemak");
+                break;
+            case 3:
+                System.out.println("You have chosen Menu Mee Goreng");
+                break;
+            default:
+                System.out.println("Your request is unavailable. Please choose your menu");
+                break;
+        }
     }
 }
