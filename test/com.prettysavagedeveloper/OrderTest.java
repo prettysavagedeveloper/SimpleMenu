1a4cb32320b7547a8b20ef2ad8be9aed67a0d05c
prettysavagedeveloper <prettysavagedeveloper@gmail.com>
2021-04-15 12:31:43 +0200

    added new test for kuih and drinks added new test for new constraint such as : - only donut is available for menu beef rendang, other kuih are not proposed

--- test/com.prettysavagedeveloper/OrderTest.java
+++ test/com.prettysavagedeveloper/OrderTest.java
@@ -68,4 +68,81 @@ public class OrderTest{
         assertEquals(output.endsWith("You have chosen Menu Beef Rendang\n"), true);
         assertEquals(output.length() > "You have chosen Menu Beef Rendang\n".length(), true );
     }
+
+    @Test
+    public void Given_KaripapAndAllKuih_When_DisplayKuihSelected_Then_DisplayKaripapSentence(){
+        order.displaySelectedKuih(1, true);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen karipap\n", output);
+    }
+
+    @Test
+    public void Given_PisangGorengAndAllKuih_When_DisplayKuihSelected_Then_DisplayPisangGorengSentence(){
+        order.displaySelectedKuih(2, true);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen pisang goreng\n", output);
+    }
+
+    @Test
+    public void Given_DonutAndAllKuih_When_DisplayKuihSelected_Then_DisplayDonutSentence(){
+        order.displaySelectedKuih(3, true);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen donut\n", output);
+    }
+
+    @Test
+    public void Given_BadValueAndAllKuih_When_DisplayKuihSelected_Then_DisplayErrorSentence(){
+        order.displaySelectedKuih(4, true);
+        String output= outContent.toString().replace("\r\n", "\n");
+        assertEquals("Unavailable\n", output);
+    }
+
+    @Test
+    public void Given_DonutAndNotAllKuih_When_DisplayKuihSelected_Then_DisplayDonutSentence(){
+        order.displaySelectedKuih(1, false);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen donut\n", output);
+    }
+
+    @Test
+    public void Given_NoDonutAndNotAllKuih_When_DispalyKuihSelected_Then_DisplayNoDonutSentence(){
+        order.displaySelectedKuih(2, false);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("No donut\n", output);
+    }
+
+    @Test
+    public void Given_BadValueAndNotAllKuih_When_DisplayKuihSelected_Then_DisplayErrorSentence(){
+        order.displaySelectedKuih(3, false);
+        String output = outContent.toString().replace("\r\n","\n");
+        assertEquals("Unavailable\n", output);
+    }
+
+    @Test
+    public void Given_TehTarikAndAllDrinks_When_DisplaySelectedDrink_Then_DisplayTehTarikSentence(){
+        order.displaySelectedDrinks(1);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen teh tarik\n", output);
+    }
+
+    @Test
+    public void Given_MiloAisAndAllDrinks_When_DisplaySelectedDrink_Then_DisplayMiloAisSentence(){
+        order.displaySelectedDrinks(2);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen milo ais\n", output);
+    }
+
+    @Test
+    public void Given_SoyaCincauAndAllDrinks_When_DisplaySelectedDrink_Then_DisplaySoyaCincauSentence(){
+        order.displaySelectedDrinks(3);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("You have chosen soya cincau\n", output);
+    }
+
+    @Test
+    public void Given_BadValueAndAllDrinks_When_DisplaySelectedDrink_Then_DisplayErrorSentence(){
+        order.displaySelectedDrinks(4);
+        String output = outContent.toString().replace("\r\n", "\n");
+        assertEquals("choice unavailable\n", output);
+    }
 }
